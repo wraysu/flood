9cc37ce496918184040df381d21fff6736801d91
Su, Wen-Ray <wraysu@gmail.com>
2021-03-19 08:33:11 +0800

    commit

--- script/dojoconfig.js
+++ script/dojoconfig.js
@@ -1,7 +1,8 @@
 var dojoConfig = {
     paths: {
         js: "/flood/script",
-        core1:"/flood/esri/core"
+        core1:"/flood/esri/core",
+        esri1:"/flood/esri"
     },
     parseOnLoad: false,
     async: false
