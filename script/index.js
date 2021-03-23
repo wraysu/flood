11266264acffb50d6afbe59948984014eb8c9f26
Su, Wen-Ray <wraysu@gmail.com>
2021-03-23 11:18:23 +0800

    commit

--- script/index.js
+++ script/index.js
@@ -28,7 +28,7 @@ require([
     center: [121, 23]
   });
 
- 
+  var ts = 0;
 
   // add the UI for a title
   view.ui.add("titleDiv", "top-right");
@@ -37,49 +37,51 @@ require([
     // around up the full time extent to full hour
     //  timeSlider.fullTimeExtent = layer.timeInfo.fullTimeExtent;
   });
-view.when(function(){
-   const timeSlider = new TimeSlider({
-    container: "timeSlider",
-    view: view,
-    mode: "instant",
-    playRate:2000,
-    timeVisible: true, // show the time stamps on the timeslider
-    fullTimeExtent: { // entire extent of the timeSlider
-      start: new Date(2019, 6, 22, 5),
-      end: new Date(2019, 6, 23, 4)
-    },
-    stops: {
-      interval: {
-        value: 1,
-        unit: "hours"
+  view.when(function () {
+    const timeSlider = new TimeSlider({
+      container: "timeSlider",
+      view: view,
+      mode: "instant",
+      playRate: 2000,
+      timeVisible: true, // show the time stamps on the timeslider
+      fullTimeExtent: { // entire extent of the timeSlider
+        start: new Date(2019, 6, 22, 5),
+        end: new Date(2019, 6, 23, 4)
+      },
+      stops: {
+        interval: {
+          value: 1,
+          unit: "hours"
+        }
+      },
+      values: [ // location of timeSlider thumbs
+        //		new Date(2000, 2, 1,0),
+        new Date(2019, 6, 22, 5)
+      ],
+      loop: true
+    });
+
+    timeSlider.watch("timeExtent", function (value) {
+      var dt = (new Date(value.end).getTime() - new Date(2019, 6, 22, 4).getTime()) / (60 * 60 * 1000)
+      console.log(dt)
+
+      var past = parseInt((pastSliderTime - new Date(2019, 6, 22, 4).getTime()) / (60 * 60 * 1000))
+      //	var t = 11950013000 + 10000 * dt
+      if (ts > 0) {
+        var url = "https://winds.ncdr.nat.gov.tw/data/flood/Frame_" + dt + ".png";
+        addFloodingLayer(url, past, dt);
       }
-    },
-    values: [ // location of timeSlider thumbs
-      //		new Date(2000, 2, 1,0),
-      new Date(2019, 6, 22, 5)
-    ],
-    loop: true
-  });
-
-  timeSlider.watch("timeExtent", function (value) {
-    var dt = (new Date(value.end).getTime() - new Date(2019, 6, 22, 4).getTime()) / (60 * 60 * 1000)
-    console.log(dt)
-
-    var past = parseInt((pastSliderTime - new Date(2019, 6, 22, 4).getTime()) / (60 * 60 * 1000))
-    //	var t = 11950013000 + 10000 * dt
-    var url = "https://winds.ncdr.nat.gov.tw/data/flood/Frame_" + dt + ".png";
-    addFloodingLayer(url, past, dt)
-    pastSliderTime = (new Date(value.end).getTime());
+      ts++;
+      pastSliderTime = (new Date(value.end).getTime());
+    })
   })
-})
- // time slider widget initialization
- 
+  // time slider widget initialization
+
 
   function addFloodingLayer(url, past, dt) {
     var pt = ((dt - 1) == 0) ? 24 : (dt - 1);
     var currLayer = view.map.findLayerById("flood" + past);
     var grapherLayer = addPictureLayer1("flood" + dt, url);
-    debugger;
     map.add(grapherLayer);
     if (currLayer) {
       setTimeout(function () { view.map.layers.remove(currLayer); }, 500)
@@ -88,9 +90,9 @@ view.when(function(){
 
   function addPictureLayer1(id, picUrl) {
     var units = "esriMeters";
-   
+
     var minPoint = webMercatorUtils.lngLatToXY(121.489563, 25.009037);
-    var maxPoint = webMercatorUtils.lngLatToXY(121.621786, 25.077316); 
+    var maxPoint = webMercatorUtils.lngLatToXY(121.621786, 25.077316);
     console.log("load Picture...");
     var dynamicLayer = new PictureLayer({
       id: id,
