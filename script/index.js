a41702a6fc520967aaa15f8760d90ebfae54bcce
Su, Wen-Ray <wraysu@gmail.com>
2021-03-23 14:58:47 +0800

    commit

--- script/index.js
+++ script/index.js
@@ -4,6 +4,7 @@ require([
   "esri/Map",
   "esri/views/MapView",
   "esri/views/SceneView",
+  "esri/layers/WebTileLayer",
   "esri/layers/FeatureLayer",
   "js/PictureLayer",
   "esri/layers/SceneLayer",
@@ -19,7 +20,7 @@ require([
   "esri/core/promiseUtils",
   "esri/widgets/Search",
   "esri/core/watchUtils"
-], function (Map, MapView, SceneView, FeatureLayer, PictureLayer, SceneLayer, webMercatorUtils, IdentityManager, TimeSlider, Expand, Legend,
+], function (Map, MapView, SceneView, WebTileLayer, FeatureLayer, PictureLayer, SceneLayer, webMercatorUtils, IdentityManager, TimeSlider, Expand, Legend,
   SketchViewModel,
   Slider,
   geometryEngine,
