6f3b904ede76cdacc326788bd5c4f62f747b8e05
Su, Wen-Ray <wraysu@gmail.com>
2021-03-23 16:00:53 +0800

    commit

--- script/index.js
+++ script/index.js
@@ -1,5 +1,27 @@
 var pastSliderTime = 0;
 var NLSCSLyrs = [];
+var isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+
+};
+
 require([
   "esri/Map",
   "esri/views/MapView",
